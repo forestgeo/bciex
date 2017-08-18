472379e0589b4093ff0bb17f4fcb93d2f260ea3a
maurolepore <maurolepore@gmail.com>
2017-08-18 14:05:05 -0400

    Move documentation of tree and stem data to data.R

--- R/data.R
+++ R/data.R
@@ -158,3 +158,43 @@
 #' * `quad`: xxx I converted this from factor to integer
 "bci_quad_info"
 
+
+
+
+
+#' Subset of tree data from Barro Colorado Island, Panama.
+#'
+#' A subset of data of 1000 randomly selected trees from Barro Colorado Island,
+#' Panama (data released in 2012). For every bciYYtNmini, "YY" and "N" indicate
+#' the year and census number. For example, bci12t1mini contains BCI data
+#' released in 2012 from census 1.
+#'
+#' @source From \url{https://repository.si.edu/handle/10088/20925}, file
+#'   bci.full#.rda.
+#'
+#' @format A dataframe with 1000 rows and 20 variables.
+#' Variables are defined at: http://ctfs.si.edu/Public/DataDict/data_dict.php.
+#' @aliases bci12t1mini bci12t2mini bci12t3mini bci12t4mini bci12t5mini
+#'   bci12t6mini bci12t7mini
+#' @name bciYYtNmini
+NULL
+
+
+# Tree and Stem Data ------------------------------------------------------
+
+#' Subset of stem data from Barro Colorado Island, Panama.
+#'
+#' Stem data corresponding to a subset of 1000 randomly selected trees from
+#' Barro Colorado Island, Panama (data released in 2012). For every bciYYsNmini,
+#' "YY" and "N" indicate the year and census number. For example, bci12s1mini
+#' contains BCI data released in 2012 from census 1.
+#'
+#' @source From \url{https://repository.si.edu/handle/10088/20925}, file
+#'   bci.stem#.rda.
+#'
+#' @format A dataframe with 2165 rows and 20 variables.
+#' Variables are defined at: http://ctfs.si.edu/Public/DataDict/data_dict.php.
+#' @aliases bci12s1mini bci12s2mini bci12s3mini bci12s4mini bci12s5mini
+#'   bci12s6mini bci12s7mini
+#' @name bciYYsNmini
+NULL

31ab8447ef6f6a4fa4d97222549d4a524148178f
maurolepore <maurolepore@gmail.com>
2018-01-08 21:56:47 -0500

    Document tree and stem tables generally.

--- R/data.R
+++ R/data.R
@@ -167,63 +167,185 @@
 
 
 
-#' Subset of tree data from Barro Colorado Island, Panama.
+# Generic tree and stem tables --------------------------------------------
+
+#' General description of census tables: tree and stem.
 #'
-#' A subset of data of 1000 randomly selected trees from Barro Colorado Island,
-#' Panama (data released in 2012). For every bciYYtNmini, "YY" and "N" indicate
-#' the year and census number. For example, bci12t1mini contains BCI data
-#' released in 2012 from census 1.
+#' Tree and stem tables are dataframes that store data from one specific census
+#' of one specific census plot. For any given plot one tree table stores data
+#' recorded during one specific census, and each row records the data from one
+#' specific tree. Similarly, for any given plot one stem table stores data
+#' recorded during one specific census , and each row records the data from one
+#' specific stem. For example, if plot X was censused twice, then it has two
+#' tree tables and two stem tables.
 #'
-#' @source From \url{https://repository.si.edu/handle/10088/20925}, file
-#'   bci.full#.rda.
+#' Each tree (or stem) table of a census plot has two remarkable properties:
+#' * The number of rows equals the number of trees (or stems) that were ever
+#' found in the plot (in any census). This implies that the number of rows is
+#' identical to that of any other tree (or stem) table of the same plot; also,
+#' that trees (or stems) appear in tree (or stem) tables before they recruit,
+#' while they are alive, and after they die.
+#' * The order of the rows is identical to that of any other tree (or stem)
+#' table of the same plot. This implies that you can compare data accross
+#' censuses even if the row order is important for your analyses. Also, you can
+#' easily repeat analyses with different censuses with minimal effort. For
+#' example, this structure makes helps to calculate demographic rates between
+#' any pair of censuses.
 #'
-#' @format A dataframe with 1000 rows and 20 variables.
-#' Variables are defined at: http://ctfs.si.edu/Public/DataDict/data_dict.php.
+#' @section Definition of Variables in tree tables:
+#'
+#' * `treeID`: The unique tree identifier in CTFS database. Useful to be certain
+#' in matching trees.
+#'
+#' * `stemID`: The unique stem identifier in CTFS database. Useful to be certain
+#'   in matching stems.
+#'
+#' * `tag`: Tag number used in the field.
+#'
+#' * `StemTag`: Tag number on the individual stem, if present.
+#'
+#' * `sp`: The species mnemonic. To get full species names, the taxonomy table
+#'   must be downloaded from the CTFS database:
+#'   http://ctfs.arnarb.harvard.edu/CTFSReports
+#'
+#' * `quadrat`: Quadrat designation.
+#'
+#' * `gx`: The x coordinate within the plot, relative to one edge of the plot.
+#'
+#' * `gy`: The y plot coordinate.
+#'
+#' * `MeasureID`: The unique identifier of a single measurement in the CTFS
+#'   database.
+#'
+#' * `CensusID`: The numeric identifier of the census.
+#'
+#' * `dbh`: Diameter of one stem on the tree, the stem whose stemID is given.
+#'
+#' * `pom`: The point-of-measure, where the diameter was taken, identical to hom,
+#'   but a character variable with only 2 decimal places.
+#'
+#' * `hom`: The height-of-measure, identical to pom but a numeric variable with
+#'   full precision.
+#'
+#' * `ExactDate`: The date on which a tree was measured.
+#'
+#' * `DFstatus`: The codes for the measurement as recorded in the field.
+#'     * `A`: Alive.
+#'     * `D`: Dead.
+#'     * `lost_stem` (deprecated): Lost stem. It usually means the stem was broken
+#'     in the given census, while the tree had no other stem.  This status is
+#'     deprecated;  it is safer to check `stemID` to determine whether a tree’s
+#'     measurement changed stems between censuses.
+#'     * `M`: Missing. It is used where there is no record of the corresponding
+#'     value of `dbh`, so it is not certain whether the tree was alive or dead.
+#'     `P`: Prior. It indicates a tree had not yet recruited at this census.
+#'
+#' * `nostems`: The number of living stems on the date of measurement.
+#'
+#' * `status`: Indicates the status of the entire tree. For example, if any stem
+#'   is alive, the tree is alive; if every stem is dead, the tree is dead:
+#'     * `A`: Aalive.
+#'     * `D`: Dead.
+#'     * `M`: Missing.
+#'     * `P`: Prior. It indicates a tree had not yet recruited at this census.
+#'     * `G`: Gone. xxx to complete.
+#'
+#' * `date`: The julian date, for date arithmetic.
+#'
+#' * `agb`: Above-ground-biomass of all stems on the tree, in Mg (equal to
+#'   metric tons or 106 g). Note that `agb = 0` for dead trees.
+#'
+#' @section Definition of Variables in stem tables:
+#' Compared to tree tables, the the variables are and mean the same. But notice
+#' some differences in `DFstatus` and `agb`; also notice the additional
+#' variables `countPOM` and `countPOM` and that the variable `nostems` does not
+#' exist for stem tables.
+#'
+#' * `treeID`: See definition of tree table.
+#'
+#' * `stemID`: See definition of tree table.
+#'
+#' * `tag`: See definition of tree table.
+#'
+#' * `StemTag`: See definition of tree table.
+#'
+#' * `sp`: See definition of tree table.
+#'
+#' * `quadrat`: See definition of tree table.
+#'
+#' * `gx`: See definition of tree table.
+#'
+#' * `gy`: See definition of tree table.
+#'
+#' * `MeasureID`: See definition of tree table.
+#'
+#' * `CensusID`: See definition of tree table.
+#'
+#' * `dbh`: Diameter of the stem.
+#'
+#' * `pom`: See definition of tree table.
+#'
+#' * `hom`: See definition of tree table.
+#'
+#' * `ExactDate`: See definition of tree table.
+#'
+#' * `DFstatus`: The codes are the same as in tree tables except that stems may
+#' have the additional status `G` (gone). `G` applies when a tree is alive and a
+#' stem which formerly had a measurement does not in this census. This happens
+#' in some plots, where individual stems do not have tags and cannot be tracked.
+#'
+#' * `codes`: The codes for the measurement as recorded in the field.
+#'
+#' * `countPOM`: The number of POMs (HOMs) for the same stem in this census.
+#'
+#' * `status`: See definition of tree table.
+#'
+#' * `date`: See definition of tree table.
+#'
+#' * `agb`: Same, but notice that some may be NA.
+#'
+#' @source \url{http://ctfs.si.edu/Public/CTFSRPackage/index.php/web/data_format}
+#' @family census tables.
+#' @name census
+NULL
+
+
+
+# Specific tree and stem tables from BCI ----------------------------------
+
+#' A randomly selected subset of tree tables from Barro Colorado Island, Panama.
+#'
+#' For every bciYYsNmini "YY" and "N" indicate the year and census number. For
+#' example, bci12t1mini contains data released in 2012 from census 1.
+#'
 #' @aliases bci12t1mini bci12t2mini bci12t3mini bci12t4mini bci12t5mini
 #'   bci12t6mini bci12t7mini
+#'
+#' @seealso census
+#'
+#' @source Files bci.full#.rdata from
+#'   \url{https://repository.si.edu/handle/10088/20925}.
+#' @family census tables.
+#' @format A dataframe with 1,000 rows and 20 variables.
 #' @name bciYYtNmini
 NULL
 
 
 
-# Tree and Stem Data ------------------------------------------------------
-
-#' Subset of stem data from Barro Colorado Island, Panama.
+#' A randomly selected subset of stem tables from Barro Colorado Island, Panama.
 #'
-#' Stem data corresponding to a subset of 1000 randomly selected trees from
-#' Barro Colorado Island, Panama (data released in 2012). For every bciYYsNmini,
-#' "YY" and "N" indicate the year and census number. For example, bci12s1mini
-#' contains BCI data released in 2012 from census 1.
-#'
-#' @source From \url{https://repository.si.edu/handle/10088/20925}, file
-#'   bci.stem#.rda.
-#'
-#' @format A dataframe with 2165 rows and 20 variables.
-#' Variables are defined at: http://ctfs.si.edu/Public/DataDict/data_dict.php.
-#'
-#' * treeID`:
-#' * `stemID`:
-#' * `tag`:
-#' * `StemTag`:
-#' * `sp`:
-#' * `quadrat`:
-#' * `gx`:
-#' * `gy`:
-#' * `MeasureID`:
-#' * `CensusID`:
-#' * `dbh`:
-#' * `pom`: Point of measurement.
-#' * `hom`: Height of measurement (xxx how is it different from pom?).
-#' * `ExactDate`:
-#' * `DFstatus`:
-#' * `codes`:
-#' * `countPOM`:
-#' * `date`:
-#' * `status`:
-#' * `agb`: `
+#' For every bciYYsNmini "YY" and "N" indicate the year and census number. For
+#' example, bci12s1mini contains data released in 2012 from census 1.
 #'
 #' @aliases bci12s1mini bci12s2mini bci12s3mini bci12s4mini bci12s5mini
 #'   bci12s6mini bci12s7mini
+#'
+#' @seealso census
+#'
+#' @source Files bci.stem#.rdata from
+#'   \url{https://repository.si.edu/handle/10088/20925}.
+#' @family census tables.
+#' @format A dataframe with 2,165 rows and 20 variables.
 #' @name bciYYsNmini
 NULL
 
